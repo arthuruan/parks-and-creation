8858bb120f1211c3c3d82bc2d9756143620bd820
Arthur Ruan <arthur123ruan@hotmail.com>
2023-10-04 21:39:11 -0300

    initial commit

---
+++ src/main.py
@@ -0,0 +1,57 @@
+import argparse
+import yaml
+from coordinates_generator import CoordinatesGenerator
+from motion_detector import MotionDetector
+from take_picture import TakePicture
+from colors import *
+import logging
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    args = parse_args()
+
+    image_file = args.image_file
+    data_file = args.data_file
+    start_frame = args.start_frame
+
+    # Take picture and saves it
+    cam = TakePicture(image_file)
+    image_saved = cam.take_picture()
+
+    if image_saved:
+        with open(data_file, "w+") as points:
+            generator = CoordinatesGenerator(image_file, points, COLOR_RED)
+            generator.generate()
+
+    with open(data_file, "r") as data:
+        print(data)
+        points = yaml.safe_load(data)
+        detector = MotionDetector(points, int(start_frame))
+        detector.detect_live_motion()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generates Coordinates File')
+
+    parser.add_argument("--image",
+                        dest="image_file",
+                        required=False,
+                        help="Image file to generate coordinates on")
+
+    parser.add_argument("--data",
+                        dest="data_file",
+                        required=True,
+                        help="Data file to be used with OpenCV")
+
+    parser.add_argument("--start-frame",
+                        dest="start_frame",
+                        required=False,
+                        default=1,
+                        help="Starting frame on the video")
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
