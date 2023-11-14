419bbc13d0a049c942d8892c18c57d50dcc07d8a
João Wallace <wallace.lucena@hotmail.com>
2023-11-13 23:52:52 -0300

    Correção no status enviado

--- src/common/motion_detector.py
+++ src/common/motion_detector.py
@@ -70,11 +70,11 @@ class MotionDetector:
 
         if occupied != self.occupied:
             diff = list(set(occupied) ^ set(self.occupied))
-            self.occupied = occupied
             for pkingLot in diff:
-                r = requests.patch(f"localhost:8080/api/vacancies/{pkingLot}", json={"status":  self.occupied})
+                r = requests.patch(f"localhost:8080/api/vacancies/{pkingLot}", json={"status": "occupied" if self.occupied else "free"})
                 if(r.status_code != 200):
                     print(f"[ERROR] Could not send parking spot {pkingLot} data!")
+            self.occupied = occupied
             print('Status: ', self.occupied)
 
         
